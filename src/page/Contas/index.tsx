348d8fdd054eb3c04e999dbde38ad74496b5e0da
Mateus Bolito <user.email>
2023-12-04 20:02:34 -0300

    refatorado componente de busines

--- src/page/Contas/index.tsx
+++ src/page/Contas/index.tsx
@@ -1,22 +1,33 @@
 import { CheckCircle } from "phosphor-react";
 import { ContasContainer, ContasContent, Conteudo } from "./styles";
-
+interface Props {
+  ProdutoName: string;
+  ContaName: string;
+}
+const busines: Props[] = [
+  {
+    ContaName: "Abre a tua conta empresarial.Fácil No teu Telefone.",
+    ProdutoName: "Conta empresarial",
+  },
+];
 export function Contas() {
   return (
     <ContasContainer>
-      <ContasContent>
-        <div>
-          <h2>Abre a tua conta empresarial.Fácil No teu Telefone.</h2>
-        </div>
-        <Conteudo>
-          <h1>Descarrega a aplicação</h1>
-          <CheckCircle size={32} />
-          <h1>Cria uma conta em minuto ‍</h1>
-          <CheckCircle size={32} />
-          <h1>Verificação concluída em algumas horas</h1>
-          <CheckCircle size={32} />
-        </Conteudo>
-      </ContasContent>
+      {busines.map((busine) => (
+        <ContasContent>
+          <div>
+            <h2>{busine.ContaName} </h2>
+          </div>
+          <Conteudo>
+            <h1>Descarrega a aplicação</h1>
+            <CheckCircle size={32} />
+            <h1>Cria uma conta em minuto ‍</h1>
+            <CheckCircle size={32} />
+            <h1>Verificação concluída em algumas horas</h1>
+            <CheckCircle size={32} />
+          </Conteudo>
+        </ContasContent>
+      ))}
     </ContasContainer>
   );
 }
